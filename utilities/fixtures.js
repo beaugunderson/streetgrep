d6f40195bd95f0fb5c65e5f9a2e497e6db0d5d40
Beau Gunderson <beau@beaugunderson.com>
2012-09-24 23:30:52 -0700

    Wrap fixtures in models.init()

--- utilities/fixtures.js
+++ utilities/fixtures.js
@@ -2,12 +2,16 @@ var util = require('util');
 
 var models = require('../models');
 
-models.User.create({
-    username: 'beau'
-  })
-  .success(function(user) {
-    console.log(util.inspect(user));
-  })
-  .error(function(err) {
-    console.error(err);
-  });
+models.init(function() {
+  console.log('models.init callback()');
+
+  models.User.create({
+      username: 'beau'
+    })
+    .success(function(user) {
+      console.log(util.inspect(user));
+    })
+    .error(function(err) {
+      console.error(err);
+    });
+});
